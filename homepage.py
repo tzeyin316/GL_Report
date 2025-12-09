3423ba25204932fd11f0941213644b81620c0217
tzeyin <tzeyin316@gmail.com>
2025-12-09 20:47:04 +0800

    Update and rename main.py to homepage.py

---
+++ homepage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="GL Report",
+    layout="wide",
+)
+
+st.title("General Ledger Report")
+
+st.sidebar.success("Select a report type.")
